2425c02f88a703dbee2ba3e6564309654e39570d
Ali <saberi.ar.eng@gmail.com>
2015-04-19 14:36:32 -0400

    temopary goodbye to factories

--- Dashboard/default/admin/app/controllers/_User.js
+++ Dashboard/default/admin/app/controllers/_User.js
@@ -1,13 +1,23 @@
 (function() {
     
-    var userController = function ($scope,userFactory) {
-        $scope.user = user;
+    var userController = function ($scope) {
+        $scope.user =  
+            {
+            "objectId": "NlPjdF5yK2",
+            "username": "saberi.ar.eng@gmail.com",
+            "emailVerified": "true",
+            "email": "saberi.ar.eng@gmail.com",
+            "myOwnCards": "['zIlHxyotrV','i1OOfskc9p','pv3NeClPkb','sdNuOoa7L2']", 
+            "name": "Alireza",
+            "profile": "Ali",
+            "surname": "Saberi",
+            "image": "../assests/images/people/NlPjdF5yK2.jpg"
+            };
 
     };
     
-    userController.$inject = ['$scope','userFactory'];
+    userController.$inject = ['$scope'];
 
-    angular.module('ccApp')
-      .controller('userController', userController);
+    angular.module('ccApp').controller('userController', userController);
     
 }());
--- Dashboard/default/admin/app/services/userFactory.js
+++ Dashboard/default/admin/app/services/userFactory.js
@@ -1,19 +1,27 @@
 (function() {
     
     var userFactory = function () {
-        user = {
-        "objectId": "NlPjdF5yK2",
-        "username": "saberi.ar.eng@gmail.com",
-        "emailVerified": "true",
-        "email": "saberi.ar.eng@gmail.com",
-        "myOwnCards": "['zIlHxyotrV','i1OOfskc9p','pv3NeClPkb','sdNuOoa7L2']", 
-        "name": "Alireza",
-        "profile": "Ali",
-        "surname": "Saberi",
-        "image": "../assests/images/people/NlPjdF5yK2.jpg"
+     
+        user = 
+            {
+            "objectId": "NlPjdF5yK2",
+            "username": "saberi.ar.eng@gmail.com",
+            "emailVerified": "true",
+            "email": "saberi.ar.eng@gmail.com",
+            "myOwnCards": "['zIlHxyotrV','i1OOfskc9p','pv3NeClPkb','sdNuOoa7L2']", 
+            "name": "Alireza",
+            "profile": "Ali",
+            "surname": "Saberi",
+            "image": "../assests/images/people/NlPjdF5yK2.jpg"
+            };
+            
+        var factory = {};
+        factory.getUser = function(){
+            return user;
         };
+
+        return factory;
     };
-        angular.module('ccApp')
-      .factory('userFactory', userFactory);
+        angular.module('ccApp').factory('userFactory', userFactory);
     
 }());
