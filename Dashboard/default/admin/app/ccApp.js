90437f4f9c1c87d796689c842d4634512b10e2c8
Ali <saberi.ar.eng@gmail.com>
2015-05-03 12:37:48 -0400

    ui-route is added but it doent work on chrome, and works on firefox

--- Dashboard/default/admin/app/ccApp.js
+++ Dashboard/default/admin/app/ccApp.js
@@ -1,3 +1,29 @@
 (function(){
-	var ccApp = angular.module("ccApp",[]);
+	var ccApp = angular.module("ccApp",['ui.router']);
+
+	ccApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('dash', {
+      url: "/dash",
+      templateUrl: "view/dashboard.html"
+    })
+    .state('pricing', {
+      url: "/pricing",
+      templateUrl: "view/pricingTable.html"
+    })
+    .state('studio', {
+      url: "/studio",
+      templateUrl: "view/portfolio.html"
+    })
+    .state('contacts', {
+      url: "/contacts",
+      templateUrl: "view/contacts.html"
+    })
+
+	});
 }());
